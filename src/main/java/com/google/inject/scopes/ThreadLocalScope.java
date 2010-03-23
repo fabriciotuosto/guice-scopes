c33c22dfb75054dc8957016a24281b1afd57b2f4
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2010-03-23 19:55:56 -0300

    change to concurrentMap

--- src/main/java/com/google/inject/scopes/ThreadLocalScope.java
+++ src/main/java/com/google/inject/scopes/ThreadLocalScope.java
@@ -1,52 +1,55 @@
 package com.google.inject.scopes;
 
-import java.util.Map;
-import java.util.WeakHashMap;
 
 import com.google.inject.Key;
 import com.google.inject.Provider;
 import com.google.inject.Scope;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class ThreadLocalScope implements Scope {
 
-	private Map<String, Object> context = new WeakHashMap<String, Object>();
+    private ConcurrentMap<String, Object> context = new ConcurrentHashMap<String, Object>();
 
-	private ThreadLocalScope() {
-		super();
-	}
+    private ThreadLocalScope() {
+        super();
+    }
+    /**
+     * Thread Local scope.
+     */
+    public static final Scope THREAD_LOCAL = new ThreadLocalScope();
 
-	/**
-	 * Thread Local scope.
-	 */
-	public static final Scope THREAD_LOCAL = new ThreadLocalScope();
+    /** A sentinel attribute value representing null. */
+    private enum NullObject {
 
-	/** A sentinel attribute value representing null. */
-	private enum NullObject {
-		INSTANCE
-	}
+        INSTANCE
+    }
 
-	@Override
-	public <T> Provider<T> scope(Key<T> key, final Provider<T> creator) {
-		final String name = key.toString();
-		return new Provider<T>() {
-			@SuppressWarnings("unchecked")
-			public T get() {
-				Object obj = context.get(name);
-				if (NullObject.INSTANCE == obj)
-					return null;
-				ThreadLocal threadLocal = (ThreadLocal) obj;
-				if (threadLocal == null) {
-					threadLocal = new ThreadLocal();
-				}
-				T t = (T) threadLocal.get();
-				if (t == null) {
-					t = creator.get();
-					threadLocal.set(t);
-					context.put(name, (t != null) ? threadLocal
-							: NullObject.INSTANCE);
-				}
-				return t;
-			}
-		};
-	}
+    @Override
+    public <T> Provider<T> scope(Key<T> key, final Provider<T> creator) {
+        final String name = key.toString();
+        return new Provider<T>() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public T get() {
+                Object obj = context.get(name);
+                if (NullObject.INSTANCE == obj) {
+                    return null;
+                }
+                ThreadLocal threadLocal = (ThreadLocal) obj;
+                if (threadLocal == null) {
+                    threadLocal = new ThreadLocal();
+                }
+                T t = (T) threadLocal.get();
+                if (t == null) {
+                    t = creator.get();
+                    threadLocal.set(t);
+                    context.put(name, (t != null) ? threadLocal
+                            : NullObject.INSTANCE);
+                }
+                return t;
+            }
+        };
+    }
 }
